b42350401580962eae0a426fbb8b9ae502ee3777
Juliana <juh.ngn@gmail.com>
2022-08-09 16:19:50 -0300

    @JuGondim [Inicializando projeto e criando contextos] {09/08/22}

---
+++ Task3/Task_3_ASP_net/Src/Modelos/Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Task_3_ASP_net.Src.Modelos
+{
+    [Table("tb_Usuarios")]
+    public class Usuario
+    {
+        #region Atributos
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id_Usuario { get; set; }
+        public string Nome { get; set; }
+        public string Log_in { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Documento { get; set; }
+        #endregion
+    }
+
+}
