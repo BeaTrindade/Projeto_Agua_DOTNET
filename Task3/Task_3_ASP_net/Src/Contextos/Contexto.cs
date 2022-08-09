b9e2e369ad6f23b7306c3ca799e9e26ec56f2d65
bpeixoto96 <peixotobruno17@gmail.com>
2022-08-09 12:11:59 -0300

    @bpeixoto96 [Inicializando projeto e criando contexto] {09/08/22}

---
+++ Task3/Task_3_ASP_net/Src/Contextos/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task_3_ASP_net.Src.Contextos
+{
+    public class Contexto : DbContext
+    {
+        #region Atributos
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Compras> Temas { get; set; }
+        public DbSet<Produtos> Postagens { get; set; }
+        #endregion
+        #region Construtores
+        public Contexto(DbContextOptions<Contexto> opt) :
+        base(opt)
+        {
+        }
+        #endregion
+    }
+}
+
